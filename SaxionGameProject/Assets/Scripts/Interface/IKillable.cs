755c2b116f8063ca22d2ba0d640ad311f81ff6f0
Thomas Oosterbeek <thomasoosterbeek@hotmail.com>
2020-12-09 12:33:06 +0100

    Added Player Stats

---
+++ SaxionGameProject/Assets/Scripts/Interface/IKillable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IKillable
+{
+   void ApplyDamage(int damage, GameObject source);
+
+   void RegisterToUIEvents();
+
+   void Die();
+}
