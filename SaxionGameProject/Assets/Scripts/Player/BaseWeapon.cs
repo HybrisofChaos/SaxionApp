f95eb77e681427ef4aa3f93437bc2de521d8d53a
Tobiascb <tobias@brouwers.in>
2021-01-13 13:19:58 +0100

    Fixed weapon

--- SaxionGameProject/Assets/Scripts/Player/BaseWeapon.cs
+++ SaxionGameProject/Assets/Scripts/Player/BaseWeapon.cs
@@ -20,11 +20,13 @@ public class BaseWeapon : MonoBehaviour
     }
 
     public GameObject[] ProjectileTypes = new GameObject[4];
-       
+    
+    
     public int playerClass;
     public int itemType;
 
-    private bool canShoot;
+    private bool canShoot = true;
+    private IEnumerator coolDown;
 
     // Start is called before the first frame update
     void Start()
@@ -32,31 +34,42 @@ public class BaseWeapon : MonoBehaviour
         
     }
 
-    public void OnFire()
+    public void OnFire() // On Fire Normal
     {
         if (canShoot)
         {
-            canShoot = false;
-            StartCoroutine("CoolDown");
-            
 
-            Vector3 rotation = transform.parent.localScale.x == 1 ? Vector3.zero : Vector3.forward * 180;
-            GameObject projectile = (GameObject)Instantiate(ProjectileTypes[itemType], transform.position * transform.parent.localScale.x, Quaternion.Euler(rotation));
 
-            projectile.GetComponent<Rigidbody2D>().velocity = transform.parent.localScale.x * Vector2.right * 100f;
+
+            Vector3 rotation = transform.localScale.x == 1 ? Vector3.zero : Vector3.forward * 180;
+            GameObject projectile = (GameObject)Instantiate(ProjectileTypes[itemType], transform.position * transform.localScale.x, Quaternion.Euler(rotation));
+            weaponStats stats = projectile.GetComponent<weaponStats>();
+            projectile.GetComponent<Rigidbody2D>().velocity = transform.localScale.x * Vector2.right * stats.projectileSpeed;
 
             /*
                 projectile.GetComponent<Rigidbody2D>().isKinematic = false;
 
                 projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.parent.localScale.x, 1) * wpn.projectileSpeed;
             */
+
+            canShoot = false;
+            coolDown = CoolDown(stats.coolDown);
+            StartCoroutine(coolDown);
+            Destroy(projectile, 3f);
         }
-        Debug.Log("Firing");
+  /*      Debug.Log("Firing");
+        Debug.Log(canShoot); */
     }
 
-    IEnumerator CoolDown()
+    public void OnSpecialFire() // On Fire Special
     {
-        yield return new WaitForSeconds(0.2f);
+        // nil
+    }
+
+
+    IEnumerator CoolDown(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
         canShoot = true;
     }
 
