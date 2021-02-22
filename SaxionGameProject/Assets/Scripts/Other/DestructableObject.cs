08760e64c4c7b6d1d2808e9d5b5f5a0c827cc640
Thomas Oosterbeek <thomasoosterbeek@hotmail.com>
2021-02-22 16:50:16 +0100

    Added script that handles basic destructableobjects

---
+++ SaxionGameProject/Assets/Scripts/Other/DestructableObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class DestructableObject : MonoBehaviour, IKillable
+{
+
+	[SerializeField]
+	private int health = 100;
+
+	[SerializeField]
+	private GameObject destroyedObject; // Object to replace this one with on death
+
+	[SerializeField]
+	private GameObject destroyParticles; // Particles to be created on death
+
+	public void ApplyDamage(int damage, GameObject source)
+	{
+		health -= damage;
+
+		if (health < 0)
+		{
+			Die();
+		}
+	}
+
+	public void Die()
+	{
+		if (destroyParticles)
+		{
+			Instantiate(destroyParticles, transform.position, Quaternion.identity);
+		}
+
+		if (destroyedObject)
+		{
+			Instantiate(destroyedObject, transform.position, Quaternion.identity);
+		}
+
+		Destroy(this.gameObject);
+	}
+
+	public void RegisterToUIEvents()
+	{
+		// NOOP
+	}
+}
